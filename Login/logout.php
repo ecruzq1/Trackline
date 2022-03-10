11379deb606ca8dce97ed16dabee72f84078599f
ecruzq1 <59055637+ecruzq1@users.noreply.github.com>
2022-03-10 15:59:03 -0500

    trackline
    io

--- Login/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset ($_SESSION['username']);
-session_destroy();
-
-echo "Gracias por utilizar el sistema.<br>";
-echo "<br><a href='../index.html'>Volver a Ingresar</a>";
-?>
